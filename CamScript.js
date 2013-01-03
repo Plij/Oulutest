db8515aca4f3d42bfabf07dcc7aac1958a17f4ee
unknown <MAtti-teppo@MAtti-teppo-PC.(none)>
2013-01-03 16:07:41 +0200

    Added CamScript for freelookcamera.

---
+++ CamScript.js
@@ -0,0 +1,51 @@
+function CamScript(entity, comp){
+  this.me = entity;
+  
+  
+  
+ 
+}
+
+function SetCam(){
+ var im = this.me.GetComponent('EC_InputMapper');
+  if(!im){
+    console.LogInfo('No camera in the scene');  
+  }else{
+    var tm = this.me.placeable.transform;
+    tm.pos.x = -92.15;
+    tm.pos.y = 23.09;
+    tm.pos.z = -73.65;
+    tm.rot.x = -19.8;
+    tm.rot.y = 263.7;
+    tm.rot.z = 0;
+    this.me.placeable.transform = tm;
+    
+    if(!this.me.inputmapper){
+      console.LogInfo('Cant find inputmapper!');
+    }else{
+      this.me.inputmapper.RemoveMapping('A', 1);
+      this.me.inputmapper.RemoveMapping('D', 1);
+      this.me.inputmapper.RemoveMapping('A', 3);
+      this.me.inputmapper.RemoveMapping('D', 3);
+      this.me.inputmapper.RemoveMapping('Space', 3);
+      this.me.inputmapper.RemoveMapping('Space', 1);
+      this.me.inputmapper.RemoveMapping('Up', 1);
+      this.me.inputmapper.RemoveMapping('Up', 3);
+      this.me.inputmapper.RemoveMapping('Down', 1);
+      this.me.inputmapper.RemoveMapping('Down', 3);
+      this.me.inputmapper.RemoveMapping('Left', 1);
+      this.me.inputmapper.RemoveMapping('Left', 3);
+      this.me.inputmapper.RemoveMapping('Right', 1);
+      this.me.inputmapper.RemoveMapping('Right', 3);
+      this.me.inputmapper.RemoveMapping('C', 1);
+      this.me.inputmapper.RemoveMapping('C', 3);
+     }
+
+  }
+
+}
+
+if (server.IsRunning())
+  console.LogInfo('CamScript loaded to FreeLookCam');
+else
+  SetCam();
